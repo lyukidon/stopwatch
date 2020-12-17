969240b210de2959d836de2267ea283a17cc220d
lyukidon <qgx99098@gmail.com>
2020-12-17 15:21:06 +0900

    start버튼을 눌렀을 때 스톱워치가 실행할 수 있도록 함

--- script.js
+++
@@ -1,6 +0,0 @@
-function time(){
-    let nowTime= new Date();
-    document.getElementById('nowTime').innerHTML = '현재 시간'+nowTime.getHours()+' : '+nowTime.getMinutes()+' : '+nowTime.getSeconds()
-} 
-
-setInterval(time,1000);
