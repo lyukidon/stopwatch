1b1d08b36f57748fd22593ad91aedb6a9c9e0acd
lyukidon <qgx99098@gmail.com>
2020-12-25 17:09:18 +0900

    style 변경

--- script/stopwatchTime.js
+++ script/stopwatchTime.js
@@ -6,18 +6,23 @@ function momentTime(){
 }
 startBtn.addEventListener('click',momentTime);
 
+let Hour;
+let Min;
+let Sec;
 function stopwatchTime(){
     let nowTime = new Date();
     let currentT = nowTime.getTime();
     let stHours = parseInt( (currentT - Time)/3600000 );
     let stMin = parseInt( ( (currentT - Time)%3600000 )/60000 );
     let stSec = parseInt( ( ( (currentT - Time)%3600000 )%60000 )/1000 )
-
+    Hour = stHours
+    Min = stMin
+    Sec = stSec
     document.getElementById('stopwatchTime').innerHTML = stHours+' : '+stMin+' : '+stSec;
 }
 //start button,stop button 관련
 let pause;
-let start = 0;
+let start = 0; //0일때 초기화 1일때 일시정지 2일때 재시작
 function onoff () {
     if (start === 0){
         pause = setInterval(stopwatchTime,1000);
@@ -30,4 +35,7 @@ function onoff () {
 
 startBtn.addEventListener('click',onoff);
 stopBtn.addEventListener('click', onoff);
-//reset button 관련
+//split button 관련
+function splitTime(){
+    splitDiv.createElement('div').innerHTML = {Hour}+{Min}+{Sec}
+}
